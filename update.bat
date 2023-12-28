0cd47e9a06064d06c14ac434309552eaff58994f
kgm423423 <kgm0610200@gmail.com>
2023-12-28 12:33:31 +0900

    2023-12-28-12:33:29.79

--- update.bat
+++ update.bat
@@ -1,4 +1,11 @@
+@echo off
+
+set var=%date%-%time%
+
 git pull origin main
+git add update.bat
 git add Workspace
-git commit -m %date% / %time%
+git commit -m %var%
 git push origin main
+
+pause>nul
