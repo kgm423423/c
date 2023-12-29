3db5e5a7aa86d7bf3e30245df353f5882ac5879a
kgm423423 <kgm0610200@gmail.com>
2023-12-29 14:02:46 +0900

    2023-12-29-14:02:44.57

--- Workspace/생명게임/생명게임1.cpp
+++ Workspace/생명게임/생명게임1.cpp
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <list>
+#include<stdlib.h>
+#include<time.h>
 
+struct cell
+{
+    ///번호
+    int num;
+
+    ///위치정보
+    int x;
+    int y;
+
+    ///특성
+    int speed;
+    int life;
+    int _size;
+};
+
+const int mapSize = 30;
+const int respawnDelay = 10;
+int mapArr[mapSize][mapSize] = {};
+list <struct cell> cells;
+
+
+void respawn(int delay)
+{
+    if (delay >= respawnDelay) {
+        ///생명 생성
+        cells.push_back()
+
+
+        delay = 0;
+    }
+    else
+        delay++;
+}
+
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+    int delay = 0;
+
+    while (true) {
+        respawn(delay);
+    }
+
+
+
+
+
+    return 0;
+}
